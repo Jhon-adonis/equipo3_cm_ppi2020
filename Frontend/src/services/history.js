bb7a8c1a32d0f210e16ad8e0d7da64faa5090542
Jhon Piñeres <pineresheriberto@gmail.com>
2020-11-03 18:38:43 -0500

    Delete Frontend

--- Frontend/src/services/history.js
+++
@@ -1,7 +0,0 @@
-/**
- * Este servicio se encarga de mantener un historial de navegación del usuario.
- * además permite añadir entradas al historial, es decir, hacerlo navegar.
- */
-import { createBrowserHistory } from 'history';
-const history = createBrowserHistory();
-export default history;
